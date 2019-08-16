20d69219f9ba676ac73664c55fec6d4401f5906b
Pedro L. Diaz Montilla <pedroluis.diaz@gmail.com>
2019-08-16 19:39:00 +0200

    Fixs an issue to avoid the app crash due to missallocation of the IOSurface

--- MusicTouch/Extension/UIImage.swift
+++ MusicTouch/Extension/UIImage.swift
@@ -14,16 +14,28 @@ extension UIImage {
     
     /// Gets the average color of an UIImage
 	var averageColor: UIColor? {
-			guard let inputImage = CIImage(image: self) else { return nil }
-			let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+		// Resizing the image fixes an issue when rendering the context. The app could't allocate the IOSurface.
+		guard let resizedImage = self.resized(toWidth: 25) else { return nil }
+		
+		guard let inputImage = CIImage(image: resizedImage) else { return nil }
+		
+		let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
 
-			guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
-			guard let outputImage = filter.outputImage else { return nil }
+		guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
+		guard let outputImage = filter.outputImage else { return nil }
 
-			var bitmap = [UInt8](repeating: 0, count: 4)
-			let context = CIContext(options: [.workingColorSpace: kCFNull])
-			context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+		var bitmap = [UInt8](repeating: 0, count: 4)
+		let context = CIContext(options: [.workingColorSpace: kCFNull!])
+		context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
 
-			return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
-		}
+		return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
+	}
+	
+    func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
 }
